89517bdf55137b92ed0b5e45664e470b8f02f17c
hongdonghyun <hong4367@gmail.com>
2017-06-25 15:18:59 +0900

    53page

--- ft.py
+++ ft.py
@@ -4,12 +4,13 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3) # sleep(3)과 같은 액션
+        self.browser.implicitly_wait(3)  # sleep(3)과 같은 액션
 
     def tearDown(self):
         self.browser.quit()
@@ -21,18 +22,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         # self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # She types "Buy pracock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
 
         # There is still a texzt box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
+        self.fail('Finish the test!')
         # The page updates again, and now show both items on ber list
 
         # Edith wonders whether the site will remember her list. Then she sees
